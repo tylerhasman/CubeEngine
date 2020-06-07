e14be1ddd72b39c719b1e0d32dfacd688a583a6a
warhead927 <49965642+warhead927@users.noreply.github.com>
2020-06-07 01:03:48 -0700

    added rotation and translation to hands when falling

--- src/main/java/me/cube/engine/game/animation/FallingAnimation.java
+++ src/main/java/me/cube/engine/game/animation/FallingAnimation.java
@@ -5,9 +5,12 @@ import org.joml.Math;
 public class FallingAnimation extends Animation {
     @Override
     public void update(Avatar avatar, float time) {
+        avatar.rotate(Avatar.BodyPart.LeftHand, (float)(1/(1+Math.exp(-5*time+3.5))), 1, 0, 0);
+        avatar.rotate(Avatar.BodyPart.RightHand, (float)(1/(1+Math.exp(-5*time+3.5))), 1, 0, 0);
+        avatar.translate(Avatar.BodyPart.LeftHand, 0, (float)(5/(1+Math.exp(-5*time+3.5))), 0);
+        avatar.translate(Avatar.BodyPart.RightHand, 0, (float)(5/(1+Math.exp(-5*time+3.5))), 0);
 
         avatar.rotate(Avatar.BodyPart.LeftLeg, Math.toRadians(45f), 1, 0, 0);
         avatar.rotate(Avatar.BodyPart.RightLeg, Math.toRadians(-45f), 1, 0, 0);
-
     }
 }
